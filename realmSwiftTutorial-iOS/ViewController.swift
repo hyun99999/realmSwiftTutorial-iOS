3f1460c2bc65d851c65f31b618350619a6ccfe4a
Hyungyu Kim <ypsophila97@icloud.com>
2021-03-01 23:25:44 +0900

    Update ViewController.swift

--- realmSwiftTutorial-iOS/ViewController.swift
+++ realmSwiftTutorial-iOS/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     var items: Results<ShoppingList>?
     var realm: Realm?
     var notificationToken: NotificationToken?
@@ -19,8 +20,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
         realm = try? Realm()
+        
         
         // shoppingList 데이터들을 가져옴
         items = realm?.objects(ShoppingList.self)
@@ -48,11 +52,16 @@ class ViewController: UIViewController {
         }
         return database
     }
+    func emptyTextField() {
+        stuffTextField.text = ""
+        priceTextField.text = ""
+    }
 //MARK: - IBAction
     @IBAction func addAction(_ sender: UIButton) {
         try! realm?.write{
             realm?.add(inputData(database: ShoppingList()))
         }
+        emptyTextField()
     }
     
     @IBAction func deleteAction(_ sender: UIButton) {
@@ -63,10 +72,12 @@ class ViewController: UIViewController {
         } catch {
             print("error")
         }
+        emptyTextField()
     }
     
     @IBAction func readAction(_ sender: UIButton) {
         print(realm?.objects(ShoppingList.self))
+        emptyTextField()
     }
     
     @IBAction func updateAction(_ sender: UIButton) {
@@ -81,6 +92,20 @@ class ViewController: UIViewController {
                 }
             })
         }
+        emptyTextField()
+    }
+    //MARK: - delegate & datasource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // deqeueResuableCell 의 반환값이 optional 이기 때문에 안전하게 처리하기 위해서 guard 문 작성.
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell") else { fatalError("error") }
+
+        cell.textLabel?.text = self.items?[indexPath.row].name
+        cell.detailTextLabel?.text = self.items?[indexPath.row].price
+        
+        return cell
     }
 }
-
